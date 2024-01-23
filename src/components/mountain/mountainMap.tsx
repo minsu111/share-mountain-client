fe576e869ec089a8ec1d08b3e215e185241be6d1
[SW5] 이수민 <tnklr1111@gmail.com>
2024-01-23 14:06:25 +0900

    feat: 네이버 지도 api 연동 & 지도 띄우고 마커 찍기

--- src/components/mountain/mountainMap.tsx
+++ src/components/mountain/mountainMap.tsx
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-const MountainMapContainer = styled.div``;
-const MountainMapBox = styled.div``;
-const MountainMapAddress = styled.div``;
+const MapContainer = styled.div`
+  width: 100%;
+`;
 
-const MountainMap = () => {
+const NaverMap = () => {
+  useEffect(() => {
+    let map = null;
+    let marker = null;
+    const initMap = () => {
+      map = new naver.maps.Map('map', {
+        center: new naver.maps.LatLng(37.34793359474108, 127.03450729483924),
+        zoom: 13,
+      });
+
+      marker = new naver.maps.Marker({
+        position: new naver.maps.LatLng(37.34793359474108, 127.03450729483924),
+        map: map,
+        icon: {
+          content: `
+            <img alt="marker" src="/assets/images/location-marker.svg" />
+          `,
+        },
+      });
+    };
+    initMap();
+  }, []);
+
+  const mapStyle = {
+    width: '100%',
+    height: '250px',
+  };
+
   return (
-    <MountainMapContainer>
-      <MountainMapBox>
-        <MountainMapAddress></MountainMapAddress>
-      </MountainMapBox>
-    </MountainMapContainer>
+    <MapContainer>
+      <div
+        id='map'
+        style={mapStyle}
+      />
+    </MapContainer>
   );
 };
 
-export default MountainMap;
+export default NaverMap;
