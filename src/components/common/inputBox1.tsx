d543bb09f951a41c9cb04df5baa83e3f7844cda9
[SW5] 이수민 <tnklr1111@gmail.com>
2024-02-10 18:56:40 +0900

    feat: 인풋 컴포넌트 - verify 버튼 추가 및 버튼 컴포넌트 일부 수정

--- src/components/common/inputBox1.tsx
+++ src/components/common/inputBox1.tsx
@@ -1,6 +1,7 @@
 // 로그인 인풋
 import React from 'react';
 import styled from 'styled-components';
+import Button from './button';
 
 const InputContainer = styled.div`
   width: 100%;
@@ -13,15 +14,22 @@ const InputContainer = styled.div`
 `;
 
 const InputLabel = styled.label`
-  width: 86%;
+  width: 88%;
   font-size: 0.9em;
   color: #474747;
+`;
+
+const InputBoxWrapper = styled.div`
+  width: 90%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const InputBox = styled.input`
   all: unset;
   padding: 0 3%;
-  width: 86%;
+  width: 100%;
   height: 2.7em;
   background-color: #eae9e9;
   border-radius: 10px;
@@ -35,7 +43,12 @@ const ValidationText = styled.div<{ validationText: string | undefined }>`
   padding: 0 6%;
   font-size: 0.8em;
   color: ${(props) =>
-    props.validationText === '사용 가능한 이메일입니다.' ? 'green' : 'red'};
+    props.validationText &&
+    ['사용 가능한 이메일입니다.', '사용 가능한 닉네임입니다.'].includes(
+      props.validationText
+    )
+      ? 'green'
+      : 'red'};
 `;
 
 interface InputProps {
@@ -45,6 +58,7 @@ interface InputProps {
   placeholder?: string;
   labelText?: string;
   validationText?: string;
+  verifyBtnText?: string;
 }
 
 const InputBox1 = ({
@@ -53,15 +67,27 @@ const InputBox1 = ({
   placeholder,
   labelText,
   validationText,
+  verifyBtnText,
 }: InputProps) => {
   return (
     <InputContainer>
       <InputLabel>{labelText}</InputLabel>
-      <InputBox
-        type={type}
-        name={name}
-        placeholder={placeholder}
-      ></InputBox>
+      <InputBoxWrapper>
+        <InputBox
+          type={type}
+          name={name}
+          placeholder={placeholder}
+        ></InputBox>
+        {verifyBtnText ? (
+          <Button
+            type='submit'
+            btnWidth='short'
+            btnText={verifyBtnText}
+            id='nickName_check'
+          />
+        ) : null}
+      </InputBoxWrapper>
+
       <ValidationText validationText={validationText}>
         {validationText}
       </ValidationText>
