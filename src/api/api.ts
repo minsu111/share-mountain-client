901fdde09e1b9df8a8f26289b499ed86fa924849
[SW5] 이수민 <tnklr1111@gmail.com>
2024-06-22 01:24:37 +0900

    feat: api 모듈 생성

---
+++ src/api/api.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { getCookie } from '@/utils/utils';
+// import { useUserStore } from '@/store/useUserStore';
+
+const origin_URL = 'http://localhost:8080';
+
+export const apiFetch = axios.create({
+  baseURL: origin_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+const isExitApplication = false;
+
+apiFetch.interceptors.request.use((config) => {
+  if (isExitApplication) {
+    throw new axios.Cancel('Application Exit!');
+  }
+
+  // const { accessToken } = useUserStore.getState();
+  const accessToken = getCookie('token');
+
+  if (accessToken) {
+    config.headers.set('Authorization', accessToken);
+  }
+
+  return config;
+});
