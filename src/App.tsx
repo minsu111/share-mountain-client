e24620f09be0fddcefb185bfa3c61a70a9877e1f
[SW5] 이수민 <tnklr1111@gmail.com>
2024-06-26 19:26:23 +0900

    feat: 토큰 만료 시 전역에서 유저 정보 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GlobalStyle } from './styles/globalStyle';
 import AppRouter from './routes/routes';
+import { useUserStore } from './store/useUserStore';
+import Cookies from 'universal-cookie';
 
 export const origin_URL = 'http://localhost:8080';
 
 function App() {
+  const cookies = new Cookies();
+
+  const { removeUserInfo } = useUserStore();
+  const token = cookies.get('token');
+
+  useEffect(() => {
+    if (!token) removeUserInfo();
+  }, []);
+
   return (
     <>
       <GlobalStyle />
